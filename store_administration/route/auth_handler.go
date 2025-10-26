56281dc278f0bd9bd8bd67802119c442fafdc4fc
vukovlevi <szabo.vukov.levente@gmail.com>
2025-10-26 21:46:50 +0100

    fix: removed unnesecary redirect in login flow

--- store_administration/route/auth_handler.go
+++ store_administration/route/auth_handler.go
@@ -40,18 +40,6 @@ func HandleLogin(c echo.Context) error {
     }
 
     SetSessionCookie(c, session)
-
-    sessionUser := ctx.Value("user")
-    user, ok := sessionUser.(model.User)
-    if !ok {
-        slog.Error("could not read user from login context")
-        return c.JSON(http.StatusInternalServerError, CreateErrorMessage("could not read user from login context")) //TODO: user-readable error message
-    }
-
-    if !user.PasswordChanged {
-        return c.Redirect(http.StatusSeeOther, "/password-change")
-    }
-
     return c.JSON(http.StatusOK, map[string]string{"message": "faca"})
 }
 
