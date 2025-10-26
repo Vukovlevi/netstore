636902020879ccd9cb38e5010c1e3112246d11b0
vukovlevi <szabo.vukov.levente@gmail.com>
2025-10-26 14:13:30 +0100

    feat: added roles

---
+++ store_administration/auth/role.go
@@ -0,0 +1,17 @@
+package auth
+
+const (
+    ROLE_STORE_LEADER = "Üzletvezető"
+    ROLE_HR = "HR"
+    ROLE_STORAGE_MANAGER = "Raktárvezető"
+    ROLE_STORAGE_HANDLER = "Raktárkezelő"
+    ROLE_CASHIER = "Pénztáros"
+    ROLE_OTHER = "Egyéb dolgozó"
+
+    ROLE_STORE_LEADER_ID = 1
+    ROLE_HR_ID = 2
+    ROLE_STORAGE_MANAGER_ID = 3
+    ROLE_STORAGE_HANDLER_ID = 4
+    ROLE_CASHIER_ID = 5
+    ROLE_OTHER_ID = 6
+)
