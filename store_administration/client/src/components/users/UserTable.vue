523cf18b752187d98c625320ffdc5906592d5889
Szabó-Vukov Levente <szabo.vukov.levente@gmail.com>
2025-11-03 09:38:25 +0100

    feat: hungarian site

--- store_administration/client/src/components/users/UserTable.vue
+++ store_administration/client/src/components/users/UserTable.vue
@@ -1,28 +1,48 @@
 <script setup lang="ts">
-import type { User } from '../../types/User';
-import UserRow from './UserRow.vue';
+import type { User } from "../../types/User";
+import UserRow from "./UserRow.vue";
 
-const { users } = defineProps<{ users: User[] }>()
+const { users } = defineProps<{ users: User[] }>();
 </script>
 
 <template>
-    <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-        <thead class="bg-gray-50 dark:bg-gray-700/50">
-            <tr>
-                <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-gray-400"
-                    scope="col">Name</th>
-                <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-gray-400"
-                    scope="col">Username</th>
-                <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-gray-400"
-                    scope="col">Phone number</th>
-                <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-gray-400"
-                    scope="col">Email</th>
-                <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-gray-400"
-                    scope="col">Role</th>
-            </tr>
-        </thead>
-        <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
-            <UserRow v-for="user in users" :user="user" />
-        </tbody>
-    </table>
+  <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+    <thead class="bg-gray-50 dark:bg-gray-700/50">
+      <tr>
+        <th
+          class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-gray-400"
+          scope="col"
+        >
+          Név
+        </th>
+        <th
+          class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-gray-400"
+          scope="col"
+        >
+          Felhasználónév
+        </th>
+        <th
+          class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-gray-400"
+          scope="col"
+        >
+          Telefonszám
+        </th>
+        <th
+          class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-gray-400"
+          scope="col"
+        >
+          Email cím
+        </th>
+        <th
+          class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500 dark:text-gray-400"
+          scope="col"
+        >
+          Rang
+        </th>
+      </tr>
+    </thead>
+    <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
+      <UserRow v-for="user in users" :user="user" />
+    </tbody>
+  </table>
 </template>
