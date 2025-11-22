da137447498dd8adf7997d5a80456c003c9c47e0
vukovlevi <szabo.vukov.levente@gmail.com>
2025-11-22 16:08:43 +0100

    fix: using correct redirect

--- store_administration/client/src/views/Home.vue
+++ store_administration/client/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mt-[5rem]">
         <h1>Home</h1>
-        <a href="http://localhost:5173/"
+        <a href="http://localhost/"
             class="rounded bg-primary px-4 py-2 text-sm font-bold text-white shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">Termek
             kezelos felulet</a>
     </div>
