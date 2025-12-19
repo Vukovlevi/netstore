5b70afb558c4816e001f2d5529684bc96cdc4111
vukovlevi <szabo.vukov.levente@gmail.com>
2025-12-19 17:31:03 +0100

    feat: server testing with client actions

--- central_server/test/server/server_test.go
+++ central_server/test/server/server_test.go
@@ -4,6 +4,7 @@ import (
 	"log/slog"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/vukovlevi/netstore/central_server/tcp"
 	client_test "github.com/vukovlevi/netstore/central_server/test/client"
@@ -19,10 +20,11 @@ func TestServer(t *testing.T) {
 	server := tcp.NewServer()
 	go server.Start()
 
-	testClientActions()
+	testClientAuthActions()
+    testClientActions()
 }
 
-func testClientActions() {
+func testClientAuthActions() {
 	client1 := client_test.NewTestClient("client1", "auth tester then bad message sender", 0)
 	client1.ConnectToServer()
 	client1.TestUnauthenticated()
@@ -32,4 +34,37 @@ func testClientActions() {
 	client1.TestAuthenticationSuccess()
 
 	client1.Close()
+}
+
+func testClientActions() {
+	client1 := client_test.NewTestClient("client1", "auth tester then bad message sender", 0)
+	client2 := client_test.NewTestClient("client2", "test client number 2", 5)
+	client3 := client_test.NewTestClient("client3", "test client number 3", 12)
+
+    client1.ConnectToServer()
+    client2.ConnectToServer()
+    client3.ConnectToServer()
+    client1.TestAuthenticationSuccess()
+    client2.TestAuthenticationSuccess()
+    client3.TestAuthenticationSuccess()
+
+    client1.SendMalformedMessage()
+    client2.SendSearchRequest()
+
+    client3.ReadClientSearch()
+    client3.SendAnswer(time.Microsecond)
+
+    client2.ReadClientAnswer(false, "client3", "test client number 3", 13)
+
+    client1.SendMalformedMessage()
+    client3.SendSearchRequest()
+
+    client2.ReadClientSearch()
+    client2.SendAnswer(time.Second * 4)
+
+    client3.ReadClientAnswer(true, "client2", "test client number 2", 6)
+
+    client1.Close()
+    client2.Close()
+    client3.Close()
 }
