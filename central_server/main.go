ba9a966860a6453556f7d01a600ae1202e0231f1
Szabó-Vukov Levente <szabo.vukov.levente@gmail.com>
2025-12-29 13:42:13 +0100

    fix: loading env variables

--- central_server/main.go
+++ central_server/main.go
@@ -4,11 +4,13 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/vukovlevi/netstore/central_server/tcp"
 )
 
 func main() {
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})))
+	godotenv.Load()
 	server := tcp.NewServer()
 	server.Start()
 }
