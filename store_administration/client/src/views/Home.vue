f5ddd3880461282b8493638a621711285eaf878a
Szabó-Vukov Levente <szabo.vukov.levente@gmail.com>
2026-03-19 09:25:28 +0100

    fix: hungarian letters on home page

--- store_administration/client/src/views/Home.vue
+++ store_administration/client/src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="mt-[5rem]">
-        <h1>Home</h1>
-        <a href="http://localhost/"
-            class="rounded bg-primary px-4 py-2 text-sm font-bold text-white shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">Termek
-            kezelos felulet</a>
-    </div>
+  <div class="mt-[5rem]">
+    <a
+      href="http://localhost/"
+      class="rounded bg-primary px-4 py-2 text-sm font-bold text-white shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+      >Termék kezelő felület
+    </a>
+  </div>
 </template>
