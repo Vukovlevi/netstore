71e07f440b5a544c4b847a119097dbfc94bdf957
vukovlevi <szabo.vukov.levente@gmail.com>
2026-03-23 16:16:01 +0100

    fix: correct env name

--- store_administration/client/src/views/Home.vue
+++ store_administration/client/src/views/Home.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-const prodAdminOrigin = import.meta.env.PROD_ADMIN_ORIGIN
+const prodAdminOrigin = import.meta.env.VITE_PROD_ADMIN_ORIGIN
 </script>
 
 <template>
     <div class="mt-[5rem]">
         <h1>Home</h1>
         <a :href="prodAdminOrigin"
-            class="rounded bg-primary px-4 py-2 text-sm font-bold text-white shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">Termek
-            kezelos felulet</a>
+            class="rounded bg-primary px-4 py-2 text-sm font-bold text-white shadow-sm hover:bg-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">Termék
+            kezelő felület</a>
     </div>
 </template>
